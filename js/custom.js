9c3eabeeca6a65ad88e6b983293dbac1b103727b
EkaterinaTolmach <turina_ekaterina@mail.ru>
2019-03-14 11:27:12 +0800

    #add Добавлена функция для вывода информации о наличии селектора на странице

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,13 @@
-
+function showSelectors(selector) {
+    var length = String(selector.length),
+        nameSelector = selector.selector;
+    return "На странице "+length+" элемент(ов) с селектором "+nameSelector;
+}
 $(function() {
     // console.log($("*")); // все селекторы
-    console.log($("[name = 'subscribe_email']")); //с атрибутом name = 'subscribe_email'
-    console.log($("[name *= 'email']")); //с атрибутом name, в значении которого есть 'email'
+    console.log(showSelectors($("[name = 'subscribe_email']"))); //с атрибутом name = 'subscribe_email'
+    console.log(showSelectors($("[name *= 'email']"))); //с атрибутом name, в значении которого есть 'email'
+    console.log(showSelectors($("input"))); //все input на странице
+    console.log(showSelectors($("button")));
+    console.log($("button"));
 });
