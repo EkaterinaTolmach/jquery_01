44a1fa45aad0b2f08862da813c89545499e2bfd8
EkaterinaTolmach <turina_ekaterina@mail.ru>
2019-03-14 17:35:33 +0800

    #add Закончена часть задания 1 про селекторы

--- js/custom.js
+++ js/custom.js
@@ -3,9 +3,20 @@ function showSelectors(selector) {
         nameSelector = selector.selector;
     return "На странице "+length+" элемент(ов) с селектором "+nameSelector;
 }
+function showSomeShitInConsole(e){
+    e.preventDefault();
+    console.log("Какая-нибудь хрень");
+}
+function showModal(e){
+    e.preventDefault();
+    console.log(e);
+    alert("Алерт!");
+    prompt("Промпт!");
+    confirm("Конферм?");
+}
 $(function() {
-    /*начало 2-ого задания*/
-    // console.log($("*")); // все селекторы
+    /*начало 2-ого пункта*/
+    console.log(showSelectors($("*"))); // все селекторы
     console.log(showSelectors($("[name = 'subscribe_email']"))); //с атрибутом name = 'subscribe_email'
     console.log(showSelectors($("[name *= 'email']"))); //с атрибутом name, в значении которого есть 'email'
     console.log(showSelectors($("input"))); //все input на странице
@@ -16,5 +27,12 @@ $(function() {
     console.log(showSelectors($('.social > *'))); //все дети тега с классом social
     console.log($('.social').children()); //все дети тега с классом social
     console.log($('#subscribe_email'));
-    /*конец 2-ого задания*/
+    /*конец 2-ого пункта*/
+
+
+    console.log(showSelectors($('[href][class]')));
+    console.log(showSelectors($('.footer-nav__link[href]')));
+    $("button").on("click",showSomeShitInConsole);
+    $("a.logo").on("click",showModal);
+
 });
