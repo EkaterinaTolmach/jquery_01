282d690c3a11a009e1ba6d7642f5255c3a51f600
EkaterinaTolmach <turina_ekaterina@mail.ru>
2019-03-14 11:00:02 +0800

    #add Начало выполнения задания по выборке

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,6 @@
 
 $(function() {
-
+    // console.log($("*")); // все селекторы
+    console.log($("[name = 'subscribe_email']")); //с атрибутом name = 'subscribe_email'
+    console.log($("[name *= 'email']")); //с атрибутом name, в значении которого есть 'email'
 });
