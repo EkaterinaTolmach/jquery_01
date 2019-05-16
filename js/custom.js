61621bef4e7c6fe35c5c64be07db17e16a059de0
EkaterinaTolmach <turina_ekaterina@mail.ru>
2019-05-16 12:36:49 +0800

    #add Инициализирован плагин wow

--- js/custom.js
+++ js/custom.js
@@ -172,4 +172,15 @@ $(function() {
 
     /**********************BXslider слайдер************************/
     $('.bxslider').bxSlider();
+
+    /*********************Анимация появления блоков steps********************/
+    var wow = new WOW(
+        {
+            boxClass:     'wow',      // animated element css class (default is wow)
+            animateClass: 'animated', // animation css class (default is animated)
+            offset:       150,          // distance to the element when triggering the animation (default is 0)
+            mobile:       false
+        }
+    );
+    wow.init();
 });
