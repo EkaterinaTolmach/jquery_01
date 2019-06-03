23be81ea151f4d5431413dc5b413c406e02d79bd
EkaterinaTolmach <turina_ekaterina@mail.ru>
2019-06-03 15:50:01 +0800

    #add Добавление инициализации яндекс карты

--- js/custom.js
+++ js/custom.js
@@ -184,4 +184,37 @@ $(function() {
         }
     );
     wow.init();
+
+    ymaps.ready(function () {
+        var myMap = new ymaps.Map('yandex-map', {
+                center: [52.279419, 104.255262],
+                zoom: 16
+            }, {
+                searchControlProvider: 'yandex#search'
+            }),
+
+            // Создаём макет содержимого.
+            MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+                '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+            ),
+
+            myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+                hintContent: 'Собственный значок метки',
+                balloonContent: 'Это красивая метка'
+            }, {
+                // Опции.
+                // Необходимо указать данный тип макета.
+                iconLayout: 'default#image',
+                // Своё изображение иконки метки.
+                iconImageHref: 'i/map-pin.png',
+                // Размеры метки.
+                iconImageSize: [40, 51],
+                // Смещение левого верхнего угла иконки относительно
+                // её "ножки" (точки привязки).
+                iconImageOffset: [-20, -51]
+            });
+        myMap.behaviors.disable('scrollZoom');
+        myMap.geoObjects
+            .add(myPlacemark);
+    });
 });
